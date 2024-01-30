eb05ec9d4b086d349332e08ce5ad494d8b29fa6f
Adao-Angelo <adaobegginer@gmail.com>
2024-01-30 21:43:51 +0100

    create listen post

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import festify from "fastify";
-
+import { pid } from "process";
+import { z } from "zod";
 const prisma = new PrismaClient();
 const app = festify();
 
@@ -9,7 +10,28 @@ app.get("/users", async () => {
   return { users };
 });
 
-app.post("/users", (request) => {
-  const { name, email } = request.body;
+app.post("/users", async (request, replay) => {
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+  });
+  const { name, email } = createUserSchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+
+  return replay.status(201).send();
 });
 
+app
+  .listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 4000,
+  })
+  .then(() => {
+    console.log("Http server running");
+  });
