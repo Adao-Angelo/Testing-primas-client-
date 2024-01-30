55963f732e2bd4779c69ea7955418c3acf381ba7
Adao-Angelo <adaobegginer@gmail.com>
2024-01-30 21:28:51 +0100

    create get user router

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
-class PrimaConfig {
-  constructor() {}
-  execute() {
-    alert("server primas strat at port 2000");
-  }
-}
-export { PrimaConfig };
+import { PrismaClient } from "@prisma/client";
+import festify from "fastify";
+
+const prisma = new PrismaClient();
+const app = festify();
+
+app.get("/users", async () => {
+  const users = await prisma.user.findMany();
+  return { users };
+});
+
+app.post("/users", (request) => {
+  const { name, email } = request.body;
+});
